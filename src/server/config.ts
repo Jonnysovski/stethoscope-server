ea8258c98b7887249d53daf6c0d11e5973369103
Johnny <yonatald@gmail.com>
2017-03-30 09:47:21 +0300

    Update zabbix url

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,7 +1,7 @@
 export default {
     ZABBIX: {
         API: {
-            URL: 'http://192.114.187.47/zabbix/api_jsonrpc.php',
+            URL: 'http://192.114.187.77/zabbix/api_jsonrpc.php',
             AUTH: {
                 ZABBIX_USERNAME: 'Admin',
                 ZABBIX_PASSWORD: 'zabbix'
